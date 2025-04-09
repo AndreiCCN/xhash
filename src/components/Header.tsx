a8b9f269ad5bd4d2987ac8e052b96c7297e75059
Nicolae-Andrei Cuconoiu <andreicuconoiu@gmail.com>
2025-04-09 22:12:50 +0300

    feat: Wallet buttons, logo & search bar design update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { client } from "@/utils/client";
-import SearchIcon from "@mui/icons-material/Search";
 import Link from "next/link";
-import { ConnectButton } from "thirdweb/react";
+import { ConnectButton, darkTheme } from "thirdweb/react";
 import { createWallet } from "thirdweb/wallets";
 import Nav from "./Nav";
+import { Search, TagTwoTone } from "@mui/icons-material";
 
 const wallets = [
   createWallet("io.metamask"),
@@ -18,25 +18,38 @@ const Header = () => {
   return (
     <header className="pt-6 bg-[#1d1d1d]">
       <div className="container flex items-center justify-between mx-auto">
-        <Link href="/" className="text-3xl font-bold">
+        <Link href="/" className="flex items-center gap-4 text-3xl font-black">
+          <div className="p-2 bg-white leading-[0.6] rounded-sm">
+            <TagTwoTone className="text-[#5e5e5e] bg-transparent rotate-45" />
+          </div>
           XHASH
         </Link>
-        <div className="w-2/5 p-2 rounded-full flex justify-between items-center gap-4 border border-[#909090]">
-          <div className="w-[50px] h-[35px] rounded-full bg-[#909090]"></div>
+        <div className="w-1/3 px-4 py-2 rounded-full flex justify-between items-center gap-2 border border-[#5e5e5e] transition-all duration-300 ease-in-out">
+          <Search />
           <input
-            className="w-full p-1 text-lg text-medium outline-none"
+            className="w-full p-1 text-lg text-medium outline-none text-center"
             placeholder="Search by address / txn hash / block"
           />
-          <SearchIcon />
         </div>
         <ConnectButton
           client={client}
           wallets={wallets}
           connectModal={{
             title: "Connect your wallet",
-            size: "wide",
+            size: "compact",
+            showThirdwebBranding: false,
           }}
           connectButton={{ label: "Connect Wallet" }}
+          theme={darkTheme({
+            colors: {
+              primaryButtonBg: "#3e3e3e",
+              primaryButtonText: "#ffffff",
+              primaryText: "#ffffff",
+              secondaryText: "#eef0f0",
+              connectedButtonBg: "#3e3e3e",
+              connectedButtonBgHover: "#474749",
+            },
+          })}
         />
       </div>
       <Nav />
